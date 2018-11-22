32b366ebde19fde44ea2945f7d18b1e03aa04f75
Vishesh <visheshsingh16@gmail.com>
2018-11-22 17:35:39 -0500

    Colors and typography

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores a, eos dolorum harum, consequuntur ea saepe obcaecati illum ullam cupiditate temporibus quibusdam quaerat iusto amet pariatur perferendis et provident mollitia!</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores a, eos dolorum harum, consequuntur ea saepe obcaecati illum ullam cupiditate temporibus quibusdam quaerat iusto amet pariatur perferendis et provident mollitia!</p>
+
+    <h1 class="display-4">Massive display</h1>
+    <h1 class="display-1">Smaller display</h1>
+    <h1 class="headline">Headline</h1>
+    <h1 class="subheading font-weight-black">Sub heading</h1>
+    <h1 class="caption">This is a caption</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+export default {};
 </script>
