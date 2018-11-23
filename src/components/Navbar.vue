4834cb47188ec0dca6a820e5fc8e550eaf0a6da2
Vishesh <visheshsingh16@gmail.com>
2018-11-23 11:47:36 -0500

    Navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <nav>
         <v-toolbar flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
             <v-toolbar-title class="grey--text text-uppercase">
                 <span class="font-weight-light">Todo</span>
                 <span>Ninja</span>
@@ -11,9 +12,19 @@
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
         </v-toolbar> 
+
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <p>test</p>
+        </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
 </script>
