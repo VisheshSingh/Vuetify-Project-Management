b182843f5160deed3c1706d1dd4f70a44f7eee0e
Vishesh <visheshsingh16@gmail.com>
2018-11-29 22:05:34 -0500

    Grid layout

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,31 +2,28 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
 
-    <v-container class="my-5">content</v-container>
-
-    <v-layout row wrap>
-      <v-flex xs12 md6>
-        <v-btn outline block class="primary">1</v-btn>
-      </v-flex>
-      <v-flex xs4 md2>
-        <v-btn outline block class="primary">2</v-btn>
-      </v-flex>
-      <v-flex xs4 md2>
-        <v-btn outline block class="primary">2</v-btn>
-      </v-flex>
-      <v-flex xs4 md2>
-        <v-btn outline block class="primary">2</v-btn>
-      </v-flex>
-    </v-layout>
-
-    <v-layout row wrap justify-space-between>
-      <v-flex xs4 md3>
-        <v-btn outline block class="success">1</v-btn>
-      </v-flex>
-      <v-flex xs4 md3>
-        <v-btn outline block class="success">2</v-btn>
-      </v-flex>
-    </v-layout>
+    <v-container class="my-5">
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          <v-flex x12 md6>
+            <div class="cation grey--text">Project Title</div>
+            <div>Create a new project</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="cation grey--text">Person</div>
+            <div>The Net Ninja</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="cation grey--text">Due by</div>
+            <div>1st January 2019</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="cation grey--text">Status</div>
+            <div>Complete</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
