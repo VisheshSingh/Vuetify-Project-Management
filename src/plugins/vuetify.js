e62e59482650490aa30f821ff427cdc47c5fed62
Vishesh <visheshsingh16@gmail.com>
2018-11-29 21:14:01 -0500

    Vuetify theme :art:

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-})
+  iconfont: "md",
+  theme: {
+    primary: "#9652ff",
+    secondary: "#3cd1d2",
+    warning: "#ffaa2c",
+    error: "#f83e70"
+  }
+});
