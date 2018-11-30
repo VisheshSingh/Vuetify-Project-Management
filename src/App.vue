bd115337f5d6e0619bac0b088b127aa0b853768f
Vishesh <visheshsingh16@gmail.com>
2018-11-29 21:38:22 -0500

    Margins & Padding

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <v-app class="grey lighten-4">
-    <Navbar />
-    <v-content>
+    <Navbar/>
+    <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
-
   </v-app>
 </template>
 
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="project">
-    <h1>Project</h1>
+    <h1 class="subheading grey--text">Project</h1>
+
+    <v-container class="my-5">content</v-container>
   </div>
 </template>
 
