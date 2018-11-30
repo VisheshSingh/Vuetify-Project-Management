72a7cf89e8c96f1726413dcbd41856543fc04a54
Vishesh <visheshsingh16@gmail.com>
2018-11-30 11:07:23 -0500

    Chips

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -18,8 +18,9 @@
             <div>{{project.due}}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
-            <div class="caption grey--text">Status</div>
-            <div>{{project.status}}</div>
+            <div class="right">
+              <v-chip small :class="`${project.status} caption white--text my-2`">{{project.status}}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
         <v-divider></v-divider>
@@ -80,5 +81,14 @@ export default {
 }
 .project.overdue {
   border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background: #3cd1c2;
+}
+.v-chip.ongoing {
+  background: #ffaa2c;
+}
+.v-chip.overdue {
+  background: #f83e70;
 }
 </style>
