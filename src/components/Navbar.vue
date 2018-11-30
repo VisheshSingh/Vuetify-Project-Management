067338d0bb603c9216fe1776645f779e665c6bd7
Vishesh <visheshsingh16@gmail.com>
2018-11-29 21:25:10 -0500

    Lists :memo:

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,43 @@
 <template>
-    <nav>
-        <v-toolbar flat app>
-            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-title class="grey--text text-uppercase">
-                <span class="font-weight-light">Todo</span>
-                <span>Ninja</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn flat color="grey">
-                <span>Sign out</span>
-                <v-icon right>exit_to_app</v-icon>
-            </v-btn>
-        </v-toolbar> 
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="grey--text text-uppercase">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
 
-        <v-navigation-drawer v-model="drawer" app class="primary">
-            <p>test</p>
-        </v-navigation-drawer>
-    </nav>
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/project" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
     };
   }
 };
