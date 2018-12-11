f97b472617b30f0cd31f33be746537dd29468565
Vishesh <visheshsingh16@gmail.com>
2018-12-11 18:23:34 -0500

    Simple form validation

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,15 +6,16 @@
         <h2>Add a new project</h2>
       </v-card-title>
       <v-card-text>
-        <v-form class="px-3">
-          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+        <v-form class="px-3" ref="form">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
           <v-menu>
             <v-text-field
               :value="formattedDate"
               label="Due date"
               slot="activator"
               prepend-icon="date_range"
+              :rules="inputRules"
             ></v-text-field>
             <v-date-picker v-model="due"></v-date-picker>
           </v-menu>
@@ -33,13 +34,16 @@ export default {
     return {
       title: "",
       content: "",
-      due: ""
+      due: "",
+      inputRules: [v => v.length >= 3 || "Minimum length is 3 characters"]
     };
   },
 
   methods: {
     submit() {
-      console.log(this.title, this.content, this.due);
+      if (this.$refs.form.validate()) {
+        console.log(this.title, this.content, this.due);
+      }
     }
   },
 
