e0593194732b67aab17d8b589dc7e6377fb3e475
Vishesh <visheshsingh16@gmail.com>
2018-12-14 11:23:46 -0500

    Button loaders

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog max-width="600px">
+  <v-dialog max-width="600px" v-model="dialog">
     <v-btn slot="activator" class="success">Add new project</v-btn>
     <v-card>
       <v-card-title>
@@ -20,7 +20,7 @@
             <v-date-picker v-model="due"></v-date-picker>
           </v-menu>
           <v-spacer></v-spacer>
-          <v-btn flat class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+          <v-btn flat class="success mx-0 mt-3" @click="submit" :loading="loading">Add project</v-btn>
         </v-form>
       </v-card-text>
     </v-card>
@@ -36,13 +36,16 @@ export default {
       title: "",
       content: "",
       due: "",
-      inputRules: [v => v.length >= 3 || "Minimum length is 3 characters"]
+      inputRules: [v => v.length >= 3 || "Minimum length is 3 characters"],
+      loading: false,
+      dialog: false
     };
   },
 
   methods: {
     submit() {
       if (this.$refs.form.validate()) {
+        this.loading = true;
         const project = {
           title: this.title,
           person: "Yoshi",
@@ -53,7 +56,10 @@ export default {
 
         db.collection("projects")
           .add(project)
-          .then(() => console.log("Project added..."));
+          .then(() => {
+            this.loading = false;
+            this.dialog = false;
+          });
       }
     }
   },
